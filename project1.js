04d750d17a78327093e42326b6c39d07abd2cbb3
Athaqadri <154593210+Athaqadri@users.noreply.github.com>
2024-01-11 17:53:59 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,39 @@
+function fun(){
+    
+
+var her = document.getElementById("her").value;
+var hey = document.getElementById("her1").value;
+var hel = document.getElementById("her2").value;
+
+if(her == "" || her == null ){
+    alert("please enter propper name");
+}
+
+
+if(hey == "" || hey == null ){
+    alert("please enter propper number");
+}
+
+
+if(hel == "" || hel == null ){
+    alert("please enter propper enail");
+}
+
+else{
+    alert("Hello! we will get in touch with u soon!");
+}
+
+
+
+}
+
+function buttton(){
+var hary = document.getElementById("but").value;
+
+if(hary == "" || hary == null){
+alert("please enter propper email");
+}
+else{
+alert("thank you");
+}
+}
